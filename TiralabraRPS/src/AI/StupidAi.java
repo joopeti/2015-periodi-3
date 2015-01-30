cded564d289403f3d183a29d8518ca48e433f034
Pyry Joona <pyry.joona@helsinki.fi>
2015-01-30 21:31:15 +0200

    luotiin tyhmä testialgoritmi toisia tekoälyjä vastaan

---
+++ TiralabraRPS/src/AI/StupidAi.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI;
+
+import GameLogic.Statistics;
+
+/**
+ *
+ * @author joopeti
+ */
+public class StupidAi extends Strategy{
+    public int[] wins = new int[]{2, 0, 1};
+    
+    @Override
+    public int predictPlayerMove() {
+        return wins[Statistics.getLastP1Move()];
+    }
+
+    @Override
+    public int predictAiMove() {
+        return wins[Statistics.getLastP2Move()];
+    }
+
+    @Override
+    public void updateModels(double decay) {
+        // Rollseyes...
+    }
+
+    @Override
+    public int getFirstRoundToUpdateModels() {
+        return 2;
+    }
+    
+}
