8e306b17bc463f8fd348281fdf913793b284d699
Pyry Joona <pyry.joona@helsinki.fi>
2015-02-25 14:23:10 +0200

    lisättiin testausta varten patternmaker-luokka

---
+++ TiralabraRPS/src/AI/strategies/PatternMaker.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI.strategies;
+
+import AI.Strategy;
+import GameLogic.Statistics;
+
+/**
+ * Antaa valintansa säännöllisinä jaksoina, joita PatternMatching yrittää huomata.
+ */
+public class PatternMaker extends Strategy{
+    private int i;
+    public PatternMaker(){
+        i = 1;
+    }
+    
+    @Override
+    public int predictPlayerMove() {
+        return (Statistics.round + i) * 7  % 3;
+    }
+
+    @Override
+    public int predictAiMove() {
+        return (Statistics.round + i) * 7 % 3;
+    }
+
+    @Override
+    public void updateModels(double decay) {
+        if(Statistics.round % 4 == 0){
+            i = (i + Statistics.round) % 1231231;
+        }
+    }
+
+    @Override
+    public int getFirstRoundToUpdateModels() {
+        return 1;
+    }
+    
+}
